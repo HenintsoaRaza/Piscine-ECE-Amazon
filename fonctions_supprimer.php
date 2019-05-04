f4e2653abb7946f8620d0e53eb5b727db184f4b4
KarlHaykal <36449162+KarlHaykal@users.noreply.github.com>
2019-05-04 18:04:10 +0200

    Add files via upload

---
+++ fonctions_supprimer.php
@@ -0,0 +1,245 @@
+<?php
+
+function affiche_photo_supprimer($nom_fichier,$db_handle){
+
+	$chaine = substr($nom_fichier, 4);
+
+	$sql = "SELECT id FROM photo WHERE nom_fichier LIKE '$chaine' " ;
+
+	$result = mysqli_query($db_handle,$sql);
+
+	$data = mysqli_fetch_assoc($result);
+
+	echo '<img src="'.$nom_fichier.'" class="image-cate" /></a>';
+
+}
+
+
+
+function apercu_image_supprimer($id,$db_handle){
+
+	$sql = "SELECT * FROM photo WHERE id LIKE $id";
+
+	$result = mysqli_query($db_handle,$sql);
+
+	$array = array();
+
+	while($data = mysqli_fetch_assoc($result)){
+
+		array_push($array, $data['nom_fichier']);
+
+	}
+
+	if(sizeof($array)!==0){affiche_photo_supprimer("img/".$array[0],$db_handle);}
+
+	else echo "Aucune photo";
+}
+
+
+
+
+function apercu_vetement_id_supprimer($id,$db_handle){
+
+	$sql = "SELECT * FROM vetement WHERE id LIKE $id";
+
+	$result = mysqli_query($db_handle,$sql);
+
+	$data = mysqli_fetch_assoc($result);
+
+	apercu_image_supprimer($id,$db_handle);
+
+	echo '
+<div class="ecriture-detail">
+<table>
+
+<tr>
+
+	<td><b>Nom : </b></td>
+
+	<td>'.$data['nom'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Description : </b></td>
+
+	<td>'.$data['description'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Prix : </b></td>
+
+	<td>'.$data['prix'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Vendeur : </b></td>
+
+	<td>'.$data['vendeur'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Type : </b></td>
+
+	<td>'.$data['type'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Sexe : </b></td>
+
+	<td>'.$data['sexe'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Vendus : </b></td>
+
+	<td>'.$data['quantite_vendue'].' </td>
+
+</tr>
+</table>
+</div>'
+;
+
+}
+
+
+function apercu_article_id_supprimer($id,$categorie,$db_handle){
+
+	$sql = "SELECT * FROM $categorie WHERE id LIKE $id";
+
+	$result = mysqli_query($db_handle,$sql);
+
+	$data = mysqli_fetch_assoc($result);
+
+	apercu_image_supprimer($id,$db_handle);
+
+	echo '
+<div class="ecriture-detail">
+<table>
+
+<tr>
+
+	<td><b>Nom : </b></td>
+
+	<td>'.$data['nom'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Description : </b></td>
+
+	<td>'.$data['description'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Prix : </b></td>
+
+	<td>'.$data['prix'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Vendeur : </b></td>
+
+	<td>'.$data['vendeur'].' </td>
+
+</tr>
+
+<tr>
+
+	<td><b>Vendus : </b></td>
+
+	<td>'.$data['quantite_vendue'].' </td>
+
+</tr>
+</table>
+</div>'
+
+;
+
+}
+
+
+
+function page_articles_supprimer($categorie,$db_handle){
+
+	if($categorie=='livre'){$sql = "SELECT id FROM livre";}
+
+	elseif ($categorie=='musique') {$sql = "SELECT id FROM musique";}
+
+	elseif ($categorie=='sport_loisir') {$sql = "SELECT id FROM sport_loisir";}
+
+	else {echo "PB in the code";}
+
+
+
+	$result = mysqli_query($db_handle,$sql);
+
+	$array = array();
+
+	while($data = mysqli_fetch_assoc($result)){
+
+		array_push($array, $data['id']);
+
+	}
+
+
+
+	for($i=0; $i<sizeof($array); $i++){
+
+		apercu_article_id_supprimer($array[$i],$categorie,$db_handle);
+		echo'<div class= "booto">
+		 <a class="btn btn-danger" href="#"><i class="fas fa-times-circle"></i> Supprimer</a> 
+		 </div>'
+		 ;
+
+	}
+
+}
+
+
+
+
+function page_vetements_supprimer($db_handle){
+
+	$sql = "SELECT id FROM vetement";
+
+	$result = mysqli_query($db_handle,$sql);
+
+	$array = array();
+
+	while($data = mysqli_fetch_assoc($result)){
+
+		array_push($array, $data['id']);
+
+	}
+
+
+
+	for($i=0; $i<sizeof($array); $i++){
+
+		apercu_vetement_id_supprimer($array[$i],$db_handle);
+		echo'<div class= "booto">
+		 <a class="btn btn-danger" href="#"><i class="fas fa-times-circle"></i> Supprimer</a> 
+		 </div>'
+		 ;
+
+	}
+
+}
+?>
---
+++ supprimer_produit_vetements.php
@@ -0,0 +1,96 @@
+<?php
+
+include("fonctions_supprimer.php");
+
+$database = "admin";
+
+$db_handle = mysqli_connect('localhost', 'root', '');
+$db_found = mysqli_select_db($db_handle, $database);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Amazon ECE</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet"
+ href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css/all.min.css">
+ <link rel="stylesheet" type="text/css" href="CssAmazon.css">
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+ $(document).ready(function(){
+ $('.header').height($(window).height());
+ });
+</script>
+
+
+</head>
+<body style="background-image: url('img/page-blanche.jpg');">
+
+
+<nav class="navbar navbar-expand-md">
+	
+ <a class="navbar-brand">
+  <img src="img/newlogo.png" alt="logo">
+</a>
+
+ <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+ <span class="navbar-toggler-icon" ></span>
+ </button>
+
+
+  <div class="collapse navbar-collapse" id="main-navigation">
+ <ul class="navbar-nav">
+
+<li class="nav-item"><a class="nav-link" href="formulaireAdmin.html"> Gerer les vendeurs </a></li>
+ <li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
+
+ 	Gerer les produits 
+ </a>
+
+ 	<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <a class="dropdown-item" href="#"> Ajouter <i class="fas fa-plus" style="color: green;"></i></a>
+    <a class="dropdown-item" href="gerer_produit_admin.html">Supprimer <i class="fas fa-times" style="color: red;"></i>
+</a>
+</div>
+</li>
+  <a class="btn btn-warning" href="admin.html"> Se deconnecter <i class="fa fa-power-off" aria-hidden="true"></i> </a>
+
+ 
+ </ul>
+ </div>
+</nav>
+
+
+ <header class="page-header header container-fluid" style="background-image: url('img/page-blanche.jpg');">	
+
+ 	<div class="overlay"></div>
+
+<div class="description">
+ <h1>Vêtements</h1>
+</div>
+
+<div class="container features">
+
+ <div class="row">
+ <div class="col-md-8">
+
+<?php 
+	page_vetements_supprimer($db_handle);
+	
+?>
+
+ </div>
+</div></div>
+</header>
+</body>
+</html>
+
+
+
